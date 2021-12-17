49592cd41037351a02a660fa99a83f0f8771ca9d
황교욱 <hkw0101@gmail.com>
2021-12-17 09:48:29 +0900

    navbar, search, sendbutton UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutterweb_ladingpage/utils/responsiveLayout.dart';
 import 'package:flutterweb_ladingpage/widgets/navbar.dart';
+import 'package:flutterweb_ladingpage/widgets/search.dart';
+import 'package:flutterweb_ladingpage/widgets/sendBtn.dart';
 
 void main() {
   runApp(
@@ -40,9 +42,7 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveLayout(
-        largeScreen: LargeChild(),
-        mediumScreen: MediumChild(),
-        smallScreen: SmallChild());
+        largeScreen: LargeChild(), smallScreen: SmallChild());
   }
 }
 
@@ -61,27 +61,48 @@ class LargeChild extends StatelessWidget {
               "assets/image_01.png",
               scale: .85,
             ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class MediumChild extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 600,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
+          ),
           FractionallySizedBox(
-            alignment: Alignment.centerRight,
+            alignment: Alignment.centerLeft,
             widthFactor: .6,
-            child: Image.network(
-              "assets/image_01.png",
-              scale: .85,
+            child: Padding(
+              padding: EdgeInsets.only(left: 48),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Hello!",
+                    style: TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Montserrat-Regular",
+                      color: Color(0xFF8591B0),
+                    ),
+                  ),
+                  RichText(
+                      text: TextSpan(
+                          text: "Wellocome To ",
+                          style:
+                              TextStyle(fontSize: 60, color: Color(0xFF8591B0)),
+                          children: [
+                        TextSpan(
+                            text: "Britu",
+                            style: TextStyle(
+                                fontSize: 60,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black87)),
+                      ])),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0, top: 20),
+                    child: Text("LET'S EXPLPORE THE WORLD"),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Search(),
+                ],
+              ),
             ),
           )
         ],
--- lib/utils/responsiveLayout.dart
+++ lib/utils/responsiveLayout.dart
@@ -12,6 +12,7 @@ class ResponsiveLayout extends StatelessWidget {
     @required this.smallScreen,
   }) : super(key: key);
 
+  //MediaQuert로 현재 해상도 사이즈 정보값으로 확인
   static bool isSmallScreen(BuildContext context) {
     return MediaQuery.of(context).size.width < 800;
   }
@@ -26,6 +27,13 @@ class ResponsiveLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //LayoutBuilder위젯은 위젯의 크기에 따라 빌드해 반응하도록 도와주는 위젯
+    //Builds a widget tree that can depend on the parent widget's size.
+    //builder function은
+    // 처음으로 widget이 layout 될 때
+    // 부모 widget의 constraint가 바뀔 때
+    // 부모 widget이 해당 widget을 업데이트할 호출한다.
+    // 즉 특정한 해상도가 변경이 될때 builder function을 호출한다.
     return LayoutBuilder(
       builder: (context, constraints) {
         if (constraints.maxWidth > 800) {
